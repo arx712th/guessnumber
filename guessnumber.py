c3848a5e0bae12900ca931269a8bcfef942cd9a2
arx712th <arx712th@gmail.com>
2021-05-30 23:25:49 +0800

    1st version

---
+++ guessnumber.py
@@ -0,0 +1,14 @@
+import random
+r = random.randint(1, 100)
+
+while True:
+	num = int(input('請輸入數字: '))
+	if num == r:
+		print('猜中了!')
+		break
+	elif num > r:
+		print('比答案大')
+	elif num < r:
+		print('比答案小')
+		break
+		
